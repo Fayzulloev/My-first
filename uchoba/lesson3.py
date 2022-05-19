829cab7a12fe701db712adba7fb55ff1d5b245ab
Fayzulloev <fayzulloev.sh@gmail.com>
2022-05-19 16:17:40 +0500

    lesson1-2-3

---
+++ uchoba/lesson3.py
@@ -0,0 +1,27 @@
+x = 15
+y = 20
+print("x = ", x)
+print("y = ", y)
+
+#Основные математический операции
+print("x + y = ", x + y)
+print("x - y = ", x - y)
+print("x * y = ", x * y)
+print("x / y = ", x / y)
+print("x % y = ", x % y)
+print("x // y = ", x // y)
+print("x ** y = ", x ** y)
+
+#Бытовые операции
+a = 5
+b = 7
+print("x = ", a, "=", bin(a))
+print("x = ", a, "=", hex(a))
+print("x = ", a, "=", hex(20))
+print("x = ", a, "=", oct(20))
+print("----------")
+
+print("x = ", a | b)
+
+
+
