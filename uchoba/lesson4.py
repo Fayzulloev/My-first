9ad1c32be0a3028a67c9435c2abe4f8fee2b1238
Fayzulloev <fayzulloev.sh@gmail.com>
2022-05-19 17:07:26 +0500

    lesson4

---
+++ uchoba/lesson4.py
@@ -0,0 +1,27 @@
+
+myster1 = "abc"
+myster2 = 'xyz'
+print("Конкатенация (соединтель) строк myster1 =", myster1 + myster2)
+
+myster = '''Длиная строка
+на много 
+строк
+'''
+print(myster)
+
+number = input("Ведите первое число: ")
+print('Вы вели: ', number)
+
+number2 = input("Ведите второе число: ")
+print('Вы вели: ', number2)
+
+print("Сумма строк: ", number + number2)
+
+print("Сумма чисел = ", int(number) + int(number2))
+
+print("Сумма чисел = ", float(number) + float(number2))
+
+chislo1 = input("Ведите первое число: ")
+chislo2 = input("Ведите второе число: ")
+chislo3 = input("Ведите третие число: ")
+print("Средное число: ", int(chislo1), int(chislo2), int(chislo3) )
