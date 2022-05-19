64954551ae0cb64232e5bbe79431be6862b42c82
Fayzulloev <fayzulloev.sh@gmail.com>
2022-05-19 15:25:26 +0500

    Lesson1

---
+++ uchoba/lesson.py
@@ -0,0 +1,23 @@
+age = 34 #integer
+print("Возраст ", age)
+
+temp = 5.9 #float
+print(temp)
+
+username = "Shukrullo" #String
+print("Имя пользователя ", username)
+
+isexists = False #bloolean
+print("Существует ", isexists)
+
+isexists2 = True #bool
+print("Существует ", isexists2)
+
+#age = 18.5
+#print("Возраст: ", age)
+
+print("Тип переменной age: ", type(age))
+print("Тип переменной temp: ", type(temp))
+print("Тип переменной username: ", type(username))
+print("Тип переменной isexists: ", type(isexists))
+
